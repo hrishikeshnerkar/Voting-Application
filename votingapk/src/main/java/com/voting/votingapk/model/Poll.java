c02081cce2908d7e42876400a64883923ca5af26
Hrishikesh Nerkar <117528304+hrishikeshnerkar@users.noreply.github.com>
2025-07-04 21:12:33 +0530

    Update Poll.java

--- votingapk/src/main/java/com/voting/votingapk/model/Poll.java
+++ votingapk/src/main/java/com/voting/votingapk/model/Poll.java
@@ -1,44 +1,7 @@
-/*package com.voting.votingapk.model;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
-
-@Entity
-@Data
-@NoArgsConstructor
-
-public class Poll {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String question;
-
-    @ElementCollection // JPA will create a separate table for this collection
-    // This is used to store the options for the poll
-    private List<String> options = new ArrayList<>();
-
-    @ElementCollection
-    private List<Long> votes = new ArrayList<>();
-
-}*/
-
-
-
-
 package com.voting.votingapk.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-        import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
